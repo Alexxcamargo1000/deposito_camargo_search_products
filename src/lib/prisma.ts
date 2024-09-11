ae865ca3fbcd25f37f974a2a7f65087d2afde2a5
ALEX CAMARGO <alexxcamargo1000@gmail.com>
2024-09-11 13:57:58 -0300

    feat: remove db local

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { Pool, neonConfig } from '@neondatabase/serverless'
+import { Pool } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
-import ws from 'ws'
 
 declare global {
   var prisma: PrismaClient | undefined;
@@ -12,7 +11,7 @@ const connectionString = `${process.env.DATABASE_URL}`
 
 const pool = new Pool({ connectionString })
 const adapter = new PrismaNeon(pool)
-const prisma = new PrismaClient({ adapter })
+const prisma = global.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
